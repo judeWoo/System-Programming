5b1c7c7c363e0507b80b3b08d493bd1f9ec7b43c
Jude <howoo@cs.stonybrook.edu>
2017-09-24 19:50:43 -0400

    Waiting Answers

--- hw2/src/main.c
+++ hw2/src/main.c
@@ -7,6 +7,9 @@ main(int argc, char *argv[])
 {
   int infile, outfile, in_flags, out_flags;
   int get_encoding_function_ret;
+  // struct stat infile_stat;
+  // struct stat outfile_stat;
+
   parse_args(argc, argv);
   check_bom();
   print_state();
@@ -14,16 +17,36 @@ main(int argc, char *argv[])
   out_flags = O_WRONLY | O_CREAT | O_TRUNC;
   infile = Open(program_state->in_file, in_flags);
   outfile = Open(program_state->out_file, out_flags);
+
+  // if (fstat(infile, &infile_stat) < 0 || fstat(outfile, &outfile_stat) < 0) {
+  //   perror("Could not stat infile");
+  //   if(program_state != NULL) {
+  //     free(program_state);
+  //   }
+  //   exit(EXIT_FAILURE);
+  // }
+  // if (infile_stat.st_ino == outfile_stat.st_ino)
+  // {
+  //   perror("In & Out are symlinked");
+  //   if(program_state != NULL) {
+  //     free(program_state);
+  //   }
+  //   exit(EXIT_FAILURE);
+  // }
+
   lseek(infile, program_state->bom_length, SEEK_SET); /* Discard BOM */
   //lseek() function allows the file offset to be set beyond the end of the file (but this does not change the size of the file).
   get_encoding_function_ret = get_encoding_function()(infile, outfile); //return function pointer and assign parameters
+
+  if(program_state != NULL) {
+    free(program_state);
+  }
+
   if (get_encoding_function_ret < 0)
   {
     return EXIT_FAILURE;
   }
-  if(program_state != NULL) {
-    free(program_state);
-  }
+
   close(outfile);
   close(infile);
 
