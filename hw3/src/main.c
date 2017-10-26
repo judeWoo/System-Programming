c1094bb401f3702a2c68d3015e7fdeac833176e6
Jude <howoo@cs.stonybrook.edu>
2017-10-25 20:11:58 -0400

    Does not matter but..

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -2,15 +2,26 @@
 #include "sfmm.h"
 #include "mysfmm.h"
 
+
+typedef struct test{
+    uint64_t      allocated;
+    uint64_t         padded;
+} Test;
+
 int main(int argc, char const *argv[]) {
+
+    Test test = {0x1234123412341234, 0x5678567856785678};
+    // Test *test_ptr = test;
 
     sf_mem_init();
 
-    void *x = sf_malloc(1);
-    void *y = sf_realloc(x, 40);
-    sf_free(y);
+    void *x = sf_malloc(32);
+    ((Test *)x)->allocated = test.allocated;
+    ((Test *)x)->padded = test.padded;
+    void *y = sf_realloc(x, 48);
 
     sf_snapshot();
+    sf_varprint(y);
 
     sf_mem_fini();
 
