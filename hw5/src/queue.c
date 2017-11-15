e3f391662222fb550f253f41d03537373f9bcf2b
Jude <howoo@cs.stonybrook.edu>
2017-11-15 00:06:23 -0500

    Part1_1

--- hw5/src/queue.c
+++ hw5/src/queue.c
@@ -1,7 +1,27 @@
 #include "queue.h"
 
 queue_t *create_queue(void) {
-    return NULL;
+    queue_t *queue;
+
+    if (queue = calloc(1, sizeof(queue_t)) == NULL)
+    {
+        perror("calloc failed");
+        exit(EXIT_FAILURE);
+    }
+    queue->front = queue->rear = NULL;
+    if ((sem_init(queue->items, 0, 0)) == -1)      //# of items 0
+    {
+        perror("sem_init failed");
+        exit(EXIT_FAILURE);
+    }
+    if (pthread_mutex_init(queue->lock, NULL) != 0)
+    {
+        perror("pthread_mutex_init failed");
+        exit(EXIT_FAILURE);
+    }
+    // queue->invalid = false;
+
+    return queue;
 }
 
 bool invalidate_queue(queue_t *self, item_destructor_f destroy_function) {
